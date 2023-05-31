c1811af6bd36162fa6cda4966438113fe04efc64
nareshdocker9447 <nk122121@gmail.com>
2023-05-31 15:42:15 +0100

    ec2

--- ec2.tf
+++ ec2.tf
@@ -1,21 +1,5 @@
-data "aws_ami" "Centos-8-DevOps-Practice" {
-  most_recent = true
-
-//  filter {
-//    name   = "name"
-//    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-//  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["872945671818"] # Canonical
-}
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.Centos-8-DevOps-Practice.id
+  ami           = "ami-0bef6cc322bfff646"
   instance_type = "t3.micro"
 
   tags = {
