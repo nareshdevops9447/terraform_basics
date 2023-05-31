d360d250a4081cb3a34c6553186023fb0bed7d15
nareshdocker9447 <nk122121@gmail.com>
2023-05-31 16:42:19 +0100

    subnet

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -26,3 +26,66 @@ resource "aws_subnet" "private" {
     Name = "private_subnet"
   }
 }
+
+# internet gateway
+
+resource "aws_internet_gateway" "automated-IGW" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "automated-IGW"
+  }
+}
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.automated-igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+# EIP
+resource "aws_eip" "auto-eip" {
+
+}
+
+# nat gateway
+resource "aws_nat_gateway" "automated-NAT" {
+  allocation_id = aws_eip.auto-eip.id
+  subnet_id     = aws_subnet.main.id
+  tags = {
+    Name = "automated-NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.automated-IGW]
+}
+
+resource "aws_route_table" "private-rt" { #for private route we don't attach IGW, we attach NAT
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.automated-NAT.id
+  }
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private-rt.id
+}
