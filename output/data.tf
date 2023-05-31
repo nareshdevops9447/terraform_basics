092a13047b6e6cddddc9d12d465c1bb8d387c00c
nareshdocker9447 <nk122121@gmail.com>
2023-05-31 21:08:48 +0100

    s3

---
+++ output/data.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "ami_info" {
+  executable_users = ["self"]
+  most_recent      = true
+  owners           = [var.aws_acc(data.aws_region.current.name)]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_region" "current" {}
+output "current_region" {
+  value = data.aws_region.current.name
+}
---
+++ output/outputs.tf
@@ -0,0 +1,7 @@
+output "ip_address" {
+  value = aws_instance.web.public_ip
+}
+
+output "ami" {
+  value = data.aws_ami.ami_info
+}
