4fb91faecb7c33ab0276fe4c9ee1228291757a78
nareshdocker9447 <nk122121@gmail.com>
2023-05-31 21:14:04 +0100

    s3

--- output/ec2.tf
+++ output/ec2.tf
@@ -2,11 +2,11 @@ resource "aws_instance" "web"{
 
   ami =  data.aws_ami.ami_info.image_id #AMI ID will be different every region, this is OS
   instance_type = "t3.micro"
-  key_name = aws_key_pair.terraform.key_name
+  # key_name = aws_key_pair.terraform.key_name
 }
 
-resource "aws_key_pair" "terraform" {
-  key_name   = "terraform"
-  # this file function will read the terraform.pub and fetch the content
-  public_key = file("${path.module}/terraform.pub")
-}
+# resource "aws_key_pair" "terraform" {
+  # key_name   = "terraform"
+ # this file function will read the terraform.pub and fetch the content
+ # public_key = file("${path.module}/terraform.pub")
+# }
