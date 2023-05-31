594d36b27ac859253f02d44d9dbca4b06da82d93
nareshdocker9447 <nk122121@gmail.com>
2023-05-31 16:09:40 +0100

    subnet

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -6,3 +6,23 @@ resource "aws_vpc" "main" {
     Name = "automated_vpc" #aws naming
   }
 }
+
+# public subnet
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+# private subnet
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private_subnet"
+  }
+}
