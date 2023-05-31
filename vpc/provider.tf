3b4950f8e74a1e38882507c9ef63b85906281b57
nareshdocker9447 <nk122121@gmail.com>
2023-05-31 18:05:27 +0100

    s3

--- vpc/provider.tf
+++ vpc/provider.tf
@@ -1,19 +1,19 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
-}
 
-backend "s3" {
-  bucket = "timing-s3"
-  key    = "timing"
-  region = "us-east-1"
-  dynamodb_table = "timing-lock"
-}
 
+  backend "s3" {
+    bucket = "timing-s3"
+    key = "timing"
+    region = "us-east-1"
+    dynamodb_table = "timing-lock"
+  }
+}
 
 # Configure the AWS Provider
 provider "aws" {
