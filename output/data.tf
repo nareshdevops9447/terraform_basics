a94e4311acd3cd377d60dae292d315a28d96b26c
nareshdocker9447 <nk122121@gmail.com>
2023-06-01 11:32:08 +0100

    s3

--- output/data.tf
+++ output/data.tf
@@ -2,17 +2,13 @@ data "aws_ami" "ami_info"{
   executable_users = ["self"]
   most_recent      = true
   # owners           = [var.aws_acc[data.aws_region.current.name]]
-
-  owners = [lookup(var.aws_acc,data.aws_region.current.name)]
+  owners = ["137112412989"]
+  # owners = [lookup(var.aws_acc,data.aws_region.current.name)]
 
   filter {
     name   = "name"
     values = ["amzn2-ami-kernel-*-x86_64-gp2"]
 
-  }
-  filter {
-    name = "owner_account_id"
-    values = ["137112412989"]
   }
 
   filter {
