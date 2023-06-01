3b102f76ea7cddc9f219672893b5f0e59bef0d04
nareshdocker9447 <nk122121@gmail.com>
2023-06-01 14:49:36 +0100

    s3

--- Count/ec2.tf
+++ Count/ec2.tf
@@ -20,3 +20,19 @@ resource "aws_instance" "test" {
   ami = "ami-0bef6cc322bfff646"
   instance_type = var.is_test ? "t2.micro" : "t3.micro"
 }
+# resource "aws_instance" "server"{
+#     for_each = var.servers #here you need to give a map to iterate
+#     ami = "ami-022d03f649d12a49d" #AMI ID will be different every region, this is OS
+#     instance_type = each.value
+#     tags = {
+#       Name = each.key
+#     }
+# }
+
+resource "aws_instance" "server"{
+  ami = "ami-0bef6cc322bfff646" #AMI ID will be different every region, this is OS
+  instance_type = "t3.micro"
+  tags = {
+    Name = local.instance_name
+  }
+}
--- Count/varaible.tf
+++ Count/varaible.tf
@@ -3,7 +3,29 @@ variable "instances" {
   default = ["web-server","app-server","db-server"]
 }
 
+# if env is test I want to create t3.micro, otherwise I want to create t3.large
 variable "is_test" {
   type = bool
-  default = true
+  default  = false
 }
+
+# web-server should be t3.micro, app-server should be t3.medium, db-server should be t3.large
+variable "servers" {
+  type = map
+  default = {
+    "web-server" = "t3.micro"
+    "app-server" = "t3.medium"
+    "db-server" = "t3.large"
+  }
+}
+
+variable "instance_name" {
+  type = string
+  default = "web-server"
+}
+
+variable "environment" {
+  type = string
+  default = "DEV"
+}
+# instance name should be web-server-DEV
