83e6d2d7c777e2e4c55311453413f7f81f47aca8
nareshdocker9447 <nk122121@gmail.com>
2023-06-01 12:55:56 +0100

    s3

--- Count/ec2.tf
+++ Count/ec2.tf
@@ -1,20 +1,20 @@
-resource "aws_instance" "web"{
-
-  # ami =  data.aws_ami.ami_info.image_id #AMI ID will be different every region, this is OS
-  ami = "ami-0bef6cc322bfff646"
-  instance_type = "t3.micro"
-  key_name = aws_key_pair.terraform.key_name
-  count=length(var.instances)
-  tags = {
-    Name = var.instances[count.index]
-  }
-}
-
-resource "aws_key_pair" "terraform" {
-  key_name   = "terraform"
- #this file function will read the terraform.pub and fetch the content
- public_key = file("${path.module}/terraform.pub")
-}
+//resource "aws_instance" "web"{
+//
+//  # ami =  data.aws_ami.ami_info.image_id #AMI ID will be different every region, this is OS
+//  ami = "ami-0bef6cc322bfff646"
+//  instance_type = "t3.micro"
+//  key_name = aws_key_pair.terraform.key_name
+//  count=length(var.instances)
+//  tags = {
+//    Name = var.instances[count.index]
+//  }
+//}
+//
+//resource "aws_key_pair" "terraform" {
+//  key_name   = "terraform"
+// #this file function will read the terraform.pub and fetch the content
+// public_key = file("${path.module}/terraform.pub")
+//}
 
 resource "aws_instance" "test" {
   ami = "ami-0bef6cc322bfff646"
